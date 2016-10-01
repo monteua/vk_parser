0b8196cd819936715c9d75014de6aea046a6e785
monteua <arximed.monte@gmail.com>
2016-10-01 18:35:46 +0300

    First commit

---
+++ cleaning.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest, time, re, getpass
+
+fhand = open('first.txt', 'r')
+fhand2 = open('second.txt', 'w')
+
+
+class Test7(unittest.TestCase):
+    def setUp(self):
+
+        self.driver = webdriver.Chrome()
+        #self.base_url = raw_input("URL: ")
+        self.accept_next_alert = True
+
+    def test_7(self):
+        driver = self.driver
+        for line in fhand:
+            driver.get(line)
+
+            lastHeight = driver.execute_script("return document.body.scrollHeight")
+
+            list_links = driver.find_elements_by_class_name('bp_text')
+            count = 1
+            for i in list_links:
+
+                link = driver.find_elements_by_class_name('mem_link')
+                for l in link:
+
+                    print count, " ", l.get_attribute('href')
+                    count +=1
+
+
+                    fhand2.write(l.get_attribute('href'))
+                    fhand2.write("\n")
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ parser.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+#Enter the link to the list of discussions
+#in the group and file will be created with all links in this discussions
+
+
+
+from selenium import webdriver
+import unittest, time, re, getpass
+
+
+fhand = open('first.txt', 'w')
+print "-".center(10, '=')
+
+
+
+class Test6(unittest.TestCase):
+    def setUp(self):
+        self.base_url = raw_input("URL: ")
+        self.driver = webdriver.Chrome()
+        #self.base_url = raw_input("URL: ")
+        self.accept_next_alert = True
+
+    def test_6(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        lastHeight = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+            newHeight = driver.execute_script("return document.body.scrollHeight")
+            if newHeight == lastHeight:
+                break
+            lastHeight = newHeight
+        list_links = driver.find_elements_by_class_name('blst_title')
+        count = 1
+        for i in list_links:
+
+            print count, " ", i.get_attribute('href')
+            count +=1
+            fhand.write(i.get_attribute('href'))
+            fhand.write("\n")
+
+if __name__ == "__main__":
+    unittest.main()
